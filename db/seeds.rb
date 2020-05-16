d243bde13f864788690f9a8b83a60c61d6f0cc35
Hector Garcia <hector.garcia07@utrgv.edu>
2020-05-15 21:22:42 -0500

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,10 @@
 User.create!(name: "Example User",
             email: "example@railstutorial.org",
             password: "foobar", password_confirmation: "foobar",
-            admin: true )
+            admin: true,
+            activated: true,
+            activated_at: Time.zone.now )
+            
 # Generate a bunch of additional users.
 99.times do |n|
     name = Faker::Name.name
@@ -10,7 +13,8 @@ User.create!(name: "Example User",
     password = "password"
     User.create!(name: name,
     email: email,
-    password:
-    password,
-    password_confirmation: password)
+    password: password,
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now )
 end
